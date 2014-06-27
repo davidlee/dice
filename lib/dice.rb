22688aec622cd7c7b9367576f43a2d01346196ae
David Lee <david.lee@envato.com>
2014-06-27 11:17:19 +1000

    tweaks

--- lib/dice.rb
+++ lib/dice.rb
@@ -9,27 +9,28 @@ module Dice
 
     def initialize(string)
       @parser   = Dice::Parser.new string
-      @resolver = Dice::Resolver.new(*parser.signature)#.tap(&:perform_rolls)
+      @resolver = Dice::Resolver.new(*parser.signature)
     end
 
     def roll
       @rolled ||= resolver.perform_rolls
     end
 
-    def display
-      roll
-      puts message
-    end
-
     def message
-      "You rolled #{resolver.rolls.inspect}".tap do |s|
-        s << " #{parser.sign} #{parser.modifier.abs}" if parser.sign
-      end + "\nTotal: #{resolver.result}"
+      roll
+      message = [].tap do |msg|
+        msg << "Rolls: #{resolver.rolls.inspect}".tap do |s|
+          s << " #{parser.sign} #{parser.modifier.abs}" if parser.sign
+        end
+        msg << "Total: #{resolver.result}"
+      end.join("\n")
     end
   end
 
   def self.roll(string)
-    Dice::Roll.new(string).display
+    puts Dice::Roll.new(string).message
+  rescue ArgumentError
+    puts "usage: roll 3d6; roll 2d10+6"
   end
 
 end
